2bf5936267730d1730779af20ba05f073024859e
Ademir Rodrigues da Silva <ademiritumbiara@outlook.com>
2020-08-27 19:51:41 -0300

    Primeiro Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,66 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import logoTop from './assets/logo-top.png';
+import LogoFullCycle from './assets/logo-fullcycle.png';
+import LogoLinkedIn from './assets/logo_linkedin.png';
+import logoGithub from './assets/logo_github.png';
+import logoTwitter from './assets/logo_twitter.png';
+
 import './App.css';
+import './styles';
+import {
+  AppContainer,
+  Header,
+  Content,
+  TextHeader,
+  Footer,
+  FooterText01,
+  FooterImagesContainer,
+  ImageFooter,
+} from './styles';
+import Routes from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <AppContainer>
+      <Header>
+        <img src={logoTop} alt="logo" />
+        <img src={LogoFullCycle} alt="logo" />
+        <TextHeader>Desafio 04 - Utilizando React.js</TextHeader>
+      </Header>
+      <Content>
+        <Routes />
+      </Content>
+      <Footer>
+        <FooterText01>Desenvolvido por</FooterText01>
+        <a href="mailto:ademiritumbiara@outlook.com">
+          Ademir Rodrigues da Silva
         </a>
-      </header>
-    </div>
+        <FooterImagesContainer>
+          <a
+            href="https://github.com/AdemirSJRP"
+            // eslint-disable-next-line react/jsx-no-target-blank
+            target="_blank"
+          >
+            <ImageFooter src={logoGithub} alt="Logo GitHub" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/ademir-rodrigues-da-silva-517a0b65/"
+            // eslint-disable-next-line react/jsx-no-target-blank
+            target="_blank"
+          >
+            <ImageFooter src={LogoLinkedIn} alt="Logo Linkedin" />
+          </a>
+          <a
+            href="https://twitter.com/ademiritumbiara"
+            // eslint-disable-next-line react/jsx-no-target-blank
+            target="_blank"
+          >
+            <ImageFooter src={logoTwitter} alt="Logo Tweeter" />
+          </a>
+        </FooterImagesContainer>
+      </Footer>
+    </AppContainer>
   );
 }
 
